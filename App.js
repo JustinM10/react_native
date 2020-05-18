636d106a577311d6bc566f4d633ae8a4814c29d3
JustinM <37004111+JustinM10@users.noreply.github.com>
2020-05-18 14:45:05 -0400

    App.js
    Main file

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import RegForm from './app/components/Regform';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+
+       <RegForm />
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#36485f',
+    paddingLeft: 60,
+    paddingRight: 60,
+
+  },
+});
